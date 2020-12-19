d7ae08a871b3b9d072645b3b3aa500137de00f10
Chrisphine10 <ondiekchrisphine@gmail.com>
2020-12-19 16:59:32 +0300

    ahl

--- mail/contact.js
+++ mail/contact.js
@@ -1,5 +1,5 @@
 $(function() {
-
+    header('Access-Control-Allow-Origin: *');
     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {},
